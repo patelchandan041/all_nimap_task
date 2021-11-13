437d799edf308687f960ad62039698271a39ecb5
Your Name <chandan@gmail.com>
2021-11-13 18:27:02 +0530

    more change in useRef form

---
+++ form_ref/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
+import Form from './Form';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Form />
+    </div>
+  );
+}
+
+export default App;
